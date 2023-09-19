691c5451d9660bccdc8831a7739d6e3942c76304
a2krocks <aakapurv@gmail.com>
2023-09-19 20:12:21 +0530

    Added firebase auth to client

--- client/a3-products-client/src/App.jsx
+++ client/a3-products-client/src/App.jsx
@@ -1,35 +1,75 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { auth } from './firebase'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleEmailChange = (e) => {
+        e.preventDefault()
+        setEmail(e.target.value)
+    }
+
+    const handlePasswordChange = (e) => {
+        e.preventDefault()
+        setPassword(e.target.value)
+    }
+``
+    const handleRegisterationSubmit = async (e) => {
+        e.preventDefault()
+        await createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in
+                const user = userCredential.user;
+                console.log(user)
+            }).catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorCode, errorMessage)
+            });
+    }
+
+    const handleLoginSubmit = async (e) => {
+        e.preventDefault();
+        await signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                console.log(userCredential);
+            }
+        ).catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode, errorMessage)
+        });
+    }
+
+    return (
+        <>
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo"/>
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo"/>
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <form className="card" onSubmit={handleLoginSubmit}>
+                <input type={"email"} value={email} onChange={handleEmailChange} />
+                <input type={"password"} value={password} onChange={handlePasswordChange} />
+                <button type={"submit"}>Login</button>
+            </form>
+            <form className="card" onSubmit={handleRegisterationSubmit}>
+                <input type={"email"} value={email} onChange={handleEmailChange} />
+                <input type={"password"} value={password} onChange={handlePasswordChange} />
+                <button type={"submit"}>Register</button>
+            </form>
+        </>
+    )
 }
 
 export default App
--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,6 @@ const authenticate = async (req, res, next) =>{
 		const token = req.headers.authorization.split(' ')[1];
 		const decodeValue = await admin.auth().verifyIdToken(token);
 		if (decodeValue) {
-			console.log(decodeValue);
 			return next();
 		}
 		return res.json({ message: 'Unauthorized' });
