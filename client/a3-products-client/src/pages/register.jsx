bbdacb0b895905a22091338a302554bbc871d731
a2krocks <aakapurv@gmail.com>
2023-09-20 19:37:02 +0530

    Few ui changes

--- client/a3-products-client/src/pages/register.jsx
+++ client/a3-products-client/src/pages/register.jsx
@@ -4,7 +4,7 @@ import Axios from 'axios'
 import Typography from '@mui/material/Typography';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import { Button, Grid, IconButton, TextField } from "@mui/material";
+import {Button, Grid, IconButton, TextField, Toolbar} from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
@@ -44,56 +44,37 @@ const SignUpPage = () => {
 
     return (
         <>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-            <link
-                rel="stylesheet"
-                href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap"
-            />
-            <meta name="viewport" content="initial-scale=1, width=device-width" />
-
             <Box sx={{ flexGrow: 1 }} >
-                <AppBar position="fixed" color="primary" sx={{ borderRadius: 4, flexGrow: 1, height: "7%", display: 'flex', flexDirection: 'row' }}>
-                    <IconButton aria-label="back" onClick={goBack} edge="start" color="inherit" sx={{
-                        width: "5%", marginTop: "0.5%"
-                    }}>
-                        <ArrowBackIcon />
-                    </IconButton>
-                    <Typography variant="h6" component="div" sx={{
-
-                        flexGrow: 1, display: "flex", flexDirection:
-                            "column", justifyContent: "center", fontWeight: "bold", fontFamily: "monospace  ", textAlign: "left", marginleft: "7%", marginTop: "0.5%"
-                    }} color="secondary">
-
-
-                        Register
-
-                    </Typography>
-
+                <AppBar position="sticky" color="primary" >
+                    <Toolbar>
+                        <IconButton aria-label="back" onClick={goBack} edge="start" color="inherit" >
+                            <ArrowBackIcon />
+                        </IconButton>
+                        <Typography variant="h6" component="div" fontFamily={"monospace"} >
+                            Register
+                        </Typography>
+                    </Toolbar>
                 </AppBar>
             </Box >
-            <div>
-
-                <form className="card" onSubmit={handleSubmit} >
-                    <Grid container direction={"column"} spacing={2}>
-                        <Grid item>
-                            <TextField type={"text"} label="First Name" value={firstName} onChange={(e) => setfirstName(e.target.value)} /><br />
-                        </Grid>
-                        <Grid item>
-                            <TextField type={"text"} label="Last Name" value={lastName} onChange={(e) => setlastName(e.target.value)} /><br />
-                        </Grid>
-                        <Grid item >
-                            <TextField type={"email"} label="E-mail ID" value={email} onChange={handleEmailChange} /><br />
-                        </Grid>
-                        <Grid item>
-                            <TextField type={"password"} label="Password" value={password} onChange={handlePasswordChange} /><br />
-                        </Grid>
-                        <Grid item>
-                            <Button variant="contained" type={"submit"}>Register</Button>
-                        </Grid>
+            <form onSubmit={handleSubmit} >
+                <Grid container direction={"column"} spacing={2} sx={{ m: 2 }}>
+                    <Grid item>
+                        <TextField type={"text"} label="First Name" required value={firstName} onChange={(e) => setfirstName(e.target.value)} /><br />
                     </Grid>
-                </form>
-            </div >
+                    <Grid item>
+                        <TextField type={"text"} label="Last Name" required value={lastName} onChange={(e) => setlastName(e.target.value)} /><br />
+                    </Grid>
+                    <Grid item >
+                        <TextField type={"email"} label="E-mail ID" required value={email} onChange={handleEmailChange} /><br />
+                    </Grid>
+                    <Grid item>
+                        <TextField type={"password"} label="Password" value={password} onChange={handlePasswordChange} /><br />
+                    </Grid>
+                    <Grid item>
+                        <Button variant="contained" type={"submit"}>Register</Button>
+                    </Grid>
+                </Grid>
+            </form>
         </>
     )
 
--- client/a3-products-client/src/theme.js
+++ client/a3-products-client/src/theme.js
@@ -2,18 +2,6 @@ import { red } from '@mui/material/colors';
 import { createTheme } from '@mui/material/styles';
 
 // A custom theme for this app
-const theme = createTheme({
-    palette: {
-        primary: {
-            main: '#e8c7fd',
-        },
-        secondary: {
-            main: '#FFFFFF',
-        },
-        error: {
-            main: red.A400,
-        },
-    },
-});
+const theme = createTheme();
 
 export default theme;
