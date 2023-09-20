cdde9204f8328952e9d2960f7d57ad8b0a63c483
ADKANK <32964280+ADKANK@users.noreply.github.com>
2023-09-20 17:20:50 +0530

    Created Registration Page

--- client/a3-products-client/src/main.jsx
+++ client/a3-products-client/src/main.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {AuthProvider} from "./context/AuthContext.jsx";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import {
     createBrowserRouter,
     RouterProvider,
 } from "react-router-dom";
 import SignInPage from "./pages/signin.jsx";
 import SignUpPage from "./pages/register.jsx";
-
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme.js';
 
 const queryClient = new QueryClient()
 const router = createBrowserRouter([
@@ -32,7 +34,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
             <AuthProvider>
-                <RouterProvider router={router} />
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <RouterProvider router={router} />
+                </ThemeProvider>,
             </AuthProvider>
         </QueryClientProvider>
     </React.StrictMode>
--- client/a3-products-client/src/pages/register.jsx
+++ client/a3-products-client/src/pages/register.jsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 import { useAuth } from "../context/AuthContext.jsx";
 import Axios from 'axios'
+import Typography from '@mui/material/Typography';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import { Button, Grid, IconButton, TextField } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 const SignUpPage = () => {
 
@@ -8,11 +14,17 @@ const SignUpPage = () => {
     const [password, setPassword] = useState('')
     const [firstName, setfirstName] = useState('');
     const [lastName, setlastName] = useState('');
-    const [phone, setPhone] = useState('');
+    const val = "Line1\nLine2";
     const { currentUser, logout, register } = useAuth()
     const handleEmailChange = (e) => {
         e.preventDefault()
         setEmail(e.target.value)
+    }
+
+
+    const navigate = useNavigate();
+    const goBack = () => {
+        navigate(-1);
     }
 
     const handlePasswordChange = (e) => {
@@ -31,18 +43,60 @@ const SignUpPage = () => {
     }
 
     return (
-        <div>
-            <h1>Sign Up Page</h1>
-            <h1>{currentUser?.email}</h1>
-            <form className="card" onSubmit={handleSubmit}>
-                <input type={"email"} value={email} onChange={handleEmailChange} />
-                <input type={"password"} value={password} onChange={handlePasswordChange} />
-                <input type={"text"} value={firstName} onChange={(e) => setfirstName(e.target.value)} />
-                <input type={"text"} value={lastName} onChange={(e) => setlastName(e.target.value)} />
-                <button type={"submit"}>Register</button>
-            </form>
-        </div>
+        <>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link
+                rel="stylesheet"
+                href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap"
+            />
+            <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+            <Box sx={{ flexGrow: 1 }} >
+                <AppBar position="fixed" color="primary" sx={{ borderRadius: 4, flexGrow: 1, height: "7%", display: 'flex', flexDirection: 'row' }}>
+                    <IconButton aria-label="back" onClick={goBack} edge="start" color="inherit" sx={{
+                        width: "5%", marginTop: "0.5%"
+                    }}>
+                        <ArrowBackIcon />
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{
+
+                        flexGrow: 1, display: "flex", flexDirection:
+                            "column", justifyContent: "center", fontWeight: "bold", fontFamily: "monospace  ", textAlign: "left", marginleft: "7%", marginTop: "0.5%"
+                    }} color="secondary">
+
+
+                        Register
+
+                    </Typography>
+
+                </AppBar>
+            </Box >
+            <div>
+
+                <form className="card" onSubmit={handleSubmit} >
+                    <Grid container direction={"column"} spacing={2}>
+                        <Grid item>
+                            <TextField type={"text"} label="First Name" value={firstName} onChange={(e) => setfirstName(e.target.value)} /><br />
+                        </Grid>
+                        <Grid item>
+                            <TextField type={"text"} label="Last Name" value={lastName} onChange={(e) => setlastName(e.target.value)} /><br />
+                        </Grid>
+                        <Grid item >
+                            <TextField type={"email"} label="E-mail ID" value={email} onChange={handleEmailChange} /><br />
+                        </Grid>
+                        <Grid item>
+                            <TextField type={"password"} label="Password" value={password} onChange={handlePasswordChange} /><br />
+                        </Grid>
+                        <Grid item>
+                            <Button variant="contained" type={"submit"}>Register</Button>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div >
+        </>
     )
+
 }
 
 export default SignUpPage
